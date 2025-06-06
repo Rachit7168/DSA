4701ada66e9b7dc60e003b206c75ebed761ceca1
Rachit7168 <rachitmakwana15@gmail.com>
2025-06-06 20:06:09 +0530

    Method 2

--- Leetcode/hash_map/Two_sum.py
+++ Leetcode/hash_map/Two_sum.py
@@ -14,3 +14,15 @@ target = 9
 
 print(twoSum(nums=nums , target=target))
 
+#method - 2 
+
+def twoSum1(nums, target):
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if i != j and nums[i] + nums[j] == target:
+                return[i ,j]
+            
+nums = [2,7,11,15]
+target = 9
+
+print(twoSum1(nums=nums , target=target))
